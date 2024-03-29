0fc9d4c183495e832d1f704dfb57501e2185d0cc
Calia21 <elechristacoal@gmail.com>
2024-03-28 17:38:59 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const header = document.querySelector(".menu-icon");
+const nav = document.querySelector('.navbar');
+ 
+function menuClick(){
+   header.addEventListener('click', ()=>{
+    // header.innerHTML = "menu-icon" ? "X" : "menu-icon";
+   nav.classList.toggle('navbar');
+ } )
+}
+ menuClick();
+
+
+function sendMessage(){
+    let button = document.querySelector('.btn');
+    button.addEventListener('click', (event)=> {
+        location.href ="https://wa.link/2ntwgl"
+    })
+}
+
+sendMessage();
+
